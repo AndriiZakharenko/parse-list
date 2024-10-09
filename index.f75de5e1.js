664bb0fdad2ed2e756b68da830c76d547f30e51c
Andrii Zakharenko <andriizakharenko@gmail.com>
2024-10-09 09:17:00 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+const salaryList = [
+    ...document.querySelectorAll("li[data-salary]")
+];
+salaryList.sort((a, b)=>{
+    const item1 = parseInt(a.getAttribute("data-salary").replace("$", ""));
+    const item2 = parseInt(b.getAttribute("data-salary").replace("$", ""));
+    return item2 - item1;
+});
+salaryList.forEach((item)=>{
+    document.querySelector("ul").appendChild(item);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
